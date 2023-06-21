3bf4e29230b5f6a69e247ff46db281e39732c1cf
Tejas013 <tejasmadhukar6@gmail.com>
2023-06-21 11:43:57 +0530

    Updated routes

--- backend/routes.py
+++ backend/routes.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import Response
+from pydantic import BaseModel
+from prisma import Prisma
 
-
+prisma = Prisma()
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_methods=["*"],
     allow_headers=["*"]
 )
 
@@ -20,6 +23,23 @@ async def shutdown_event():
 
 @app.get("/")
 async def root():
-    res = Response("Welcome to Api", status_code=200)
+    res = Response("Api is up and running!!", status_code=200)
+    return res
+
+class SignupRequest(BaseModel):
+    email: str
+    password: str
+    name: str
+
+@app.post('/signup')
+async def signup(request: SignupRequest):
+    _user = prisma.user.create(
+        data={
+            'name': request.name,
+            'email': request.email,
+            'password': request.password
+        }
+    )
+    res = Response("Signup Successful", status_code=201)
     return res
 
