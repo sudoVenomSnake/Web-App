2cd2953ab0be0a5fc3ee174efdf845b0c43f719e
Tejas013 <tejasmadhukar6@gmail.com>
2023-06-21 11:08:54 +0530

    Added prisma

--- backend/routes.py
+++ backend/routes.py
@@ -2,12 +2,21 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import Response
 
+
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_headers=["*"]
 )
+
+@app.on_event("startup")
+async def startup_event():
+    await prisma.connect()
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    await prisma.disconnect()
 
 @app.get("/")
 async def root():
