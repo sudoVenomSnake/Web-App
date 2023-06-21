260641b833d2a978577e44043c8e44d8dd8e458c
oreotamish <sinhatamish@gmail.com>
2023-06-21 15:04:50 +0530

    Co-authored-by: Tejasmadhukar <Tejasmadhukar@users.noreply.github.com>

--- backend/routes.py
+++ backend/routes.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import Response
-from pydantic import BaseModel
 from prisma import Prisma
+from passlib.context import CryptContext
+from pydantic import BaseModel
+import jwt
+import uvicorn
 
 prisma = Prisma()
 app = FastAPI()
@@ -26,20 +29,33 @@ async def root():
     res = Response("Api is up and running!!", status_code=200)
     return res
 
-class SignupRequest(BaseModel):
+class UserCreate(BaseModel):
+    name: str
     email: str
     password: str
-    name: str
 
 @app.post('/signup')
-async def signup(request: SignupRequest):
-    _user = prisma.user.create(
-        data={
-            'name': request.name,
-            'email': request.email,
-            'password': request.password
-        }
-    )
+async def signup(user: UserCreate):
+    hashed_password = get_password_hash(user.password)
+    try:
+        created_user =await prisma.user.create(
+            data={
+                'name': user.name,
+                'email': user.email,
+                'password': hashed_password
+            }
+        )
+    except Exception as e:
+        return Response(e, status_code=500)
+          
     res = Response("Signup Successful", status_code=201)
     return res
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def get_password_hash(password: str):
+    return pwd_context.hash(password)
+
+if __name__ == "__main__":
+    uvicorn.run("routes:app",port=8000, reload=True)
