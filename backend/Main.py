92a60d1cb817c9f6b6e6fe639d6ec6732eec1ef3
sudoVenomSnake <swastikm26@gmail.com>
2023-06-25 00:20:48 +0530

    Functionality in Python

--- backend/Main.py
+++ backend/Main.py
@@ -1,193 +1,151 @@
-import streamlit as st
-import redirect as rd
-
-import os
-import tempfile
-import time
-
-from llama_index import SimpleDirectoryReader, StorageContext, LLMPredictor
-from llama_index import TreeIndex
-from llama_index import ServiceContext
-from langchain.prompts import StringPromptTemplate
-from typing import List, Union
-from langchain.schema import AgentAction, AgentFinish
-from langchain.agents import AgentExecutor, LLMSingleActionAgent, AgentOutputParser
-from langchain import LLMChain, OpenAI
-from llama_index.indices.tree.tree_root_retriever import TreeRootRetriever
-import re
+from dotenv import load_dotenv
+from llama_index import VectorStoreIndex, SimpleDirectoryReader, ServiceContext
+from llama_index.vector_stores import PineconeVectorStore
+from llama_index.storage.storage_context import StorageContext
 from langchain.chat_models import ChatOpenAI
-from llama_index.tools import QueryEngineTool, ToolMetadata
-from llama_index.query_engine import SubQuestionQueryEngine
-from langchain.agents import Tool
-from llama_index.query_engine import RetrieverQueryEngine
-# import nest_asyncio
+from llama_index.llm_predictor import LLMPredictor
+from langchain.agents import ZeroShotAgent, Tool, AgentExecutor
+from langchain import OpenAI, SerpAPIWrapper, LLMChain
+import pinecone
+from huggingface_hub.inference_api import InferenceApi
+import os
+import openai
 
-# nest_asyncio.apply()
-os.environ['OPENAI_API_KEY'] = os.environ['API_KEY']
-query_engine_tools = []
+load_dotenv()
 
-import asyncio
-def get_or_create_eventloop():
-    try:
-        return asyncio.get_event_loop()
-    except RuntimeError as ex:
-        if "There is no current event loop in thread" in str(ex):
-            loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(loop)
-            return asyncio.get_event_loop()
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def remove_formatting(output):
-    output = re.sub('\[[0-9;m]+', '', output)  
-    output = re.sub('\', '', output) 
-    return output.strip()
+pinecone.init(environment = "asia-southeast1-gcp-free")
 
-@st.cache_resource
-def preprocessing(uploaded_files):
-    names = []
-    descriptions = []
-    if uploaded_files:
-        temp_dir = tempfile.TemporaryDirectory()
-        file_paths = []
-        
-        for uploaded_file in uploaded_files:
-            file_path = os.path.join(temp_dir.name, uploaded_file.name)
-            with open(file_path, "wb") as f:
-                f.write(uploaded_file.read())
-            file_paths.append(file_path)
-        
-        for file_path in file_paths:
-            document = SimpleDirectoryReader(input_files=[file_path]).load_data()
-            index = TreeIndex.from_documents(document)
-            engine = index.as_query_engine(similarity_top_k = 3)
-            retriever = TreeRootRetriever(index)
-            temp_engine = RetrieverQueryEngine(retriever=retriever)
-            summary = temp_engine.query("Write a short concise summary of this document")
-            heading = temp_engine.query("Write a short concise heading of this document")
-            description = str(summary)
-            name = str(heading)
-            query_engine_tools.append(QueryEngineTool(
-                query_engine = engine,
-                metadata = ToolMetadata(name = name, description = description)
-            ))
-            names.append(name)
-            descriptions.append(description)
-        st.write(names)
-        st.write(descriptions)
+tools = []
 
-        s_engine = SubQuestionQueryEngine.from_defaults(query_engine_tools = query_engine_tools)
+### Replace output.show() with frontends
 
-        tools = [Tool(
-            name = "Llama-Index",
-            func = s_engine.query,
-            description = f"Useful for when you want to answer questions on the topics - {names}. The input to this tool should be a complete English sentence.",
+def waifu(prompt):
+    inference = InferenceApi(repo_id = "hakurei/waifu-diffusion")
+    output = (inference(prompt))
+    output.show()
+    return "The image was generated and displayed."
+
+def midjourney(prompt):
+    inference = InferenceApi(repo_id = "prompthero/openjourney") #Use the activation token mdjrny-v4 style in prompt
+    output = (inference(prompt))
+    output.show()
+    return "The image was generated and displayed."
+
+def disney(prompt):
+    inference = InferenceApi(repo_id = "nitrosocke/mo-di-diffusion") #Use the activation token modern disney style in prompt
+    output = (inference(prompt))
+    output.show()
+    return "The image was generated and displayed."
+
+def real(prompt):
+    inference = InferenceApi(repo_id = "dreamlike-art/dreamlike-photoreal-2.0") #Use the activation token photo in prompt
+    output = (inference(prompt))
+    output.show()
+    return "The image was generated and displayed."
+
+def timeless(prompt):
+    inference = InferenceApi(repo_id = "wavymulder/timeless-diffusion") #Use the activation token timeless style in prompt
+    output = (inference(prompt))
+    output.show()
+    return "The image was generated and displayed."
+
+### To be called at the beginning of the chat process, to be called ONLY ONCE, creates a new Pinecone Index, deletes the old, preprocesses the documents.
+def preprocessing_prelimnary(name = "", description = ""):
+    path_to_temp = r'temp'
+    documents = SimpleDirectoryReader(path_to_temp).load_data()
+    pinecone_index = pinecone.Index('best')
+    pinecone_index.delete(deleteAll = True)
+    vector_store = PineconeVectorStore(pinecone_index = pinecone_index)
+    storage_context = StorageContext.from_defaults(vector_store = vector_store)
+    llm_predictor_chatgpt = LLMPredictor(llm = ChatOpenAI(temperature = 0, model_name = "gpt-3.5-turbo"))
+    service_context = ServiceContext.from_defaults(chunk_size = 128, llm_predictor = llm_predictor_chatgpt)
+    index = VectorStoreIndex.from_documents(documents, service_context = service_context, storage_context = storage_context)
+    engine = index.as_query_engine(similarity_top_k = 3)
+    tool_description = engine.query('Give a very brief and concise description of this document. A maximum of 2 or 3 sentences.')
+    print(tool_description)
+    search = SerpAPIWrapper()
+    tools = [Tool(
+            name = "Documents",
+            func = engine.query,
+            description = f"{tool_description}. The input to this tool should be a complete English sentence. Use this tool if don't have context.",
             return_direct = True
-            )
-        ]
+            ),
+            Tool(
+            name = "Search",
+            func = search.run,
+            description="useful for when you need to answer questions about current events",
+            ),
+            Tool(
+            name = "Anime Image Generation",
+            func = waifu,
+            description = "useful for generating anime images. The input to this tool should be descriptions/features separated by a comma. Do give identifying features like ethnicity, age, hair color et cetera for better results.",
+            ),
+            # Tool(
+            # name = "Anime Image Generation",
+            # func = waifu,
+            # description = "useful for generating anime images. The input to this tool should be descriptions/features separated by a comma. Do give identifying features like ethnicity, age, hair color et cetera for better results.",
+            # ),
+            # Tool(
+            # name = "Anime Image Generation",
+            # func = waifu,
+            # description = "useful for generating anime images. The input to this tool should be descriptions/features separated by a comma. Do give identifying features like ethnicity, age, hair color et cetera for better results.",
+            # ),
+            # Tool(
+            # name = "Anime Image Generation",
+            # func = waifu,
+            # description = "useful for generating anime images. The input to this tool should be descriptions/features separated by a comma. Do give identifying features like ethnicity, age, hair color et cetera for better results.",
+            # )
+    ]
+    if not name:
+        if not description:
+            prefix = """You are an AI Assistant. Answer the following questions as best you can. You have access to the following tools:"""
+            suffix = """You are rewarded for using the Documnets tool, use it as much as you can. Remember to be moral and ethical. Reply in the language you were asked the question in. Begin."
 
-        template1 = """You are an Assistant. You have access to the following tools:
+            Question: {input}
+            {agent_scratchpad}"""
+        else:
+            prefix = f"""You are an AI Assistant. A brief description about you - {description}. Answer the following questions as best you can. You have access to the following tools:"""
+            suffix = """You are rewarded for using the Documnets tool, use it as much as you can. Remember to be moral and ethical. Reply in the language you were asked the question in. Begin."
 
-                    {tools}
+            Question: {input}
+            {agent_scratchpad}"""
+    else:
+        if not description:
+            prefix = f"""Your name is {name}. Stay in character. Answer the following questions as best you can. You have access to the following tools:"""
+            suffix = """You are rewarded for using the Documnets tool, use it as much as you can. Remember to be moral and ethical. Reply in the language you were asked the question in. Begin."
 
-                    Use the following format:
+            Question: {input}
+            {agent_scratchpad}"""
+        else:
+            prefix = f"""Your name is {name} and a brief description about you is {description}. Stay in character. Answer the following questions as best you can. You have access to the following tools:"""
+            suffix = """You are rewarded for using the Documnets tool, use it as much as you can. Remember to be moral and ethical. Reply in the language you were asked the question in. Begin."
 
-                    Question: the input question you must answer
-                    Thought: you should always think about what to do
-                    Action: the action to take, should be one of [{tool_names}]
-                    Action Input: the input to the action
-                    Observation: the result of the action
-                    ... (this Thought/Action/Action Input/Observation can repeat N times)
-                    Thought: I now know the final answer
-                    Final Answer: the final answer to the original input question
+            Question: {input}
+            {agent_scratchpad}"""
 
-                    Begin! Remember to be ethical and articulate when giving your final answer. Use lots of "Arg"s
+    prompt = ZeroShotAgent.create_prompt(
+        tools, prefix = prefix, suffix = suffix, input_variables = ["input", "agent_scratchpad"]
+    )
 
-                    Question: {input}
-                    {agent_scratchpad}"""
+    print(prompt.template)
 
-        prompt = CustomPromptTemplate(
-            template = template1,
-            tools = tools,
-            input_variables=["input", "intermediate_steps"]
-        )
+    llm_chain = LLMChain(llm = OpenAI(temperature = 0), prompt = prompt)
 
-        output_parser = CustomOutputParser()
+    tool_names = [tool.name for tool in tools]
 
-        llm = OpenAI(temperature = 0)
-        llm_chain = LLMChain(llm = llm, prompt = prompt)
+    agent = ZeroShotAgent(llm_chain = llm_chain, allowed_tools = tool_names)
 
-        tool_names = [tool.name for tool in tools]
-        agent = LLMSingleActionAgent(
-            llm_chain = llm_chain, 
-            output_parser = output_parser,
-            stop = ["\nObservation:"], 
-            allowed_tools = tool_names
-        )
+    global agent_executor
+    agent_executor = AgentExecutor.from_agent_and_tools(
+        agent = agent, tools = tools, verbose = True
+    )
 
-        agent_chain = AgentExecutor.from_agent_and_tools(tools = tools, agent = agent, verbose = True)
+### To run a querys
+def run(question):
+    response = agent_executor.run(question)
+    print(response)
 
-        return agent_chain
-    
-@st.cache_resource
-def run(query):
-    if query:
-        with rd.stdout() as out:
-            ox = agent_chain.run(query)
-        output = out.getvalue()
-        output = remove_formatting(output)
-        st.write(ox.response)
-        return True
-
-class CustomPromptTemplate(StringPromptTemplate):
-    template: str
-    tools: List[Tool]
-    
-    def format(self, **kwargs) -> str:
-        intermediate_steps = kwargs.pop("intermediate_steps")
-        thoughts = ""
-        for action, observation in intermediate_steps:
-            thoughts += action.log
-            thoughts += f"\nObservation: {observation}\nThought: "
-        kwargs["agent_scratchpad"] = thoughts
-        kwargs["tools"] = "\n".join([f"{tool.name}: {tool.description}" for tool in self.tools])
-        kwargs["tool_names"] = ", ".join([tool.name for tool in self.tools])
-        return self.template.format(**kwargs)
-    
-class CustomOutputParser(AgentOutputParser):
-    
-    def parse(self, llm_output: str) -> Union[AgentAction, AgentFinish]:
-        if "Final Answer:" in llm_output:
-            return AgentFinish(
-                return_values={"output": llm_output.split("Final Answer:")[-1].strip()},
-                log=llm_output,
-            )
-        regex = r"Action\s*\d*\s*:(.*?)\nAction\s*\d*\s*Input\s*\d*\s*:[\s]*(.*)"
-        match = re.search(regex, llm_output, re.DOTALL)
-        if not match:
-            raise ValueError(f"Could not parse LLM output: `{llm_output}`")
-        action = match.group(1).strip()
-        action_input = match.group(2)
-        return AgentAction(tool=action, tool_input=action_input.strip(" ").strip('"'), log=llm_output)
-
-st.set_page_config(layout = "wide")
-
-st.title("Tools and Subqueries using LangChain and Llama-Index")
-st.write("Upload your PDF files")
-
-llm_predictor = LLMPredictor(llm = ChatOpenAI(temperature = 0, model_name = 'gpt-3.5-turbo', max_tokens = -1))
-
-storage_context = StorageContext.from_defaults()
-service_context = ServiceContext.from_defaults(llm_predictor = llm_predictor)
-
-uploaded_files = st.file_uploader("Upload files", accept_multiple_files = True)
-
-agent_chain = preprocessing(uploaded_files)
-ack = False
-
-if agent_chain:
-    query = st.text_input('Enter your Query.', key = 'query_input')
-    ack = run(query)
-    if ack:
-        ack = False
-        query = st.text_input('Enter your Query.', key = 'new_query_input')
-        ack = run(query)
-        
+preprocessing_prelimnary('AI Bot', 'AI Assistant for answering questions.')
+run('Generate an anime image of Tamish based on his attributes')
